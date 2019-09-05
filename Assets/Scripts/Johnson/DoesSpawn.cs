693730ff5382fecc51de01e1d0789ee1dfe93b03
Unknown <johnk117@ferris.edu>
2019-09-05 11:19:33 -0400

    Built Grey Box Level for ChunkJohnson
    ~Built a grey box level for ChunkJohnson. Everything I built I put in their respective areas in a folder labelled Johnson.
    
    +rock prefab
    +boulder prefab
    +ramp prefab
    +wall prefab
    +DoesSpawn Script

---
+++ Assets/Scripts/Johnson/DoesSpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoesSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        float picker = Random.Range(1f, 10f);
+
+        if(picker <= 5f)
+        {
+            gameObject.SetActive(true);
+        }
+        else if(picker >= 5f)
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+}
