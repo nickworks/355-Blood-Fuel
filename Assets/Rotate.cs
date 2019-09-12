6b0e9d9481cf96c61210c609f9236007806b17f9
Unknown <whitel24@ferris.edu>
2019-09-11 22:49:40 -0400

    Programmed the tower to have random y rotation when it spawns

---
+++ Assets/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    void Start()
+    {
+        //float yAngle = Random.Range(-180, 180f);
+        //transform.Rotate(0, yAngle, 0);
+
+        var euler = transform.eulerAngles;
+        euler.y = Random.Range(0f, 360f);
+        transform.eulerAngles = euler;
+    }
+}
