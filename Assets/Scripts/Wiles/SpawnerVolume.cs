000704fb922342e90edcfef2a17fcb479fc97e2f
Jacob Wiles <Jacobtwiles1226@gmail.com>
2019-09-12 11:44:06 -0400

    Started setting up Spawing Volume Prefab
    +Created a TODO List in Spawner Volume Script
    *re-positioned the volume prefab to be centered in its' own local transform.

--- Assets/Scripts/Wiles/SpawnerVolume.cs
+++ Assets/Scripts/Wiles/SpawnerVolume.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class SpawnerVolume : MonoBehaviour
 {
+    private Vector3 volumeBounds;
+    GameObject volume;
+
     // Start is called before the first frame update
     void Start()
     {
         
+        //TODO: Disable this object's mesh renderer and collider
+        //TODO: Figure out this object's size
+        //TODO: Figure out what other object(s) this object will spawn
+        //TODO: Figure out the size of the ojects from the previous step
+        //TODO: Spawn objects inside this object to fill up it's space but not clipping into each other. at least not too much
+
+        volumeBounds = new Vector3()
+
     }
 
     // Update is called once per frame
