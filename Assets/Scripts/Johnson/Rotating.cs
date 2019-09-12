690b61d2ab0bbfdde34e9f1406d35a63eccc34f7
Keegan <johnk117@ferris.edu>
2019-09-12 13:04:37 -0400

    Did more Grey Boxing and added Set piece
    ~ Did a little more grey boxing to level
    
    +mountain temple prefab as the set piece for level
    +Script which rotates set piece to make it more interesting
    *ChunkJohnson

---
+++ Assets/Scripts/Johnson/Rotating.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomRotation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // rotates prefab by a random 90 degrees
+    void RandomRotation()
+    {
+        float randPicker = Random.Range(0f, 1f);
+
+        if (randPicker < .3f)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        if (randPicker > .3f && randPicker < .5f)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (randPicker > .5f && randPicker < .7f)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 270, 0);
+        }
+        if (randPicker > .7f)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 360, 0);
+        }
+
+    }
+}
