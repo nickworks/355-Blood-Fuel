cec34f62fe466949c5a99ebcf889eafba9e17ff1
Jacob Wiles <Jacobtwiles1226@gmail.com>
2019-09-12 12:48:05 -0400

    Resolved Compiler Errors in someone's code
    * ... Jacob was missing a ";" at the end of one of his lines. It's there now.

--- Assets/Scripts/Wiles/SpawnerVolume.cs
+++ Assets/Scripts/Wiles/SpawnerVolume.cs
@@ -10,14 +10,14 @@ public class SpawnerVolume : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
         //TODO: Disable this object's mesh renderer and collider
         //TODO: Figure out this object's size
         //TODO: Figure out what other object(s) this object will spawn
         //TODO: Figure out the size of the ojects from the previous step
         //TODO: Spawn objects inside this object to fill up it's space but not clipping into each other. at least not too much
 
-        volumeBounds = new Vector3()
+        volumeBounds = new Vector3();
 
     }
 
