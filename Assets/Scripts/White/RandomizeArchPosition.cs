a290523eee68c98d6b823a60bff86a65f867e719
Unknown <whitel24@ferris.edu>
2019-09-18 19:51:40 -0400

    Created an arch that spawns with a random x position

---
+++ Assets/Scripts/White/RandomizeArchPosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeArchPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var xPosition = transform.position;
+        xPosition.x = Random.Range(-200f, 200f);
+        transform.position = xPosition;
+    }
+}
--- Assets/Scripts/White/White_Rotate.cs
+++ Assets/Scripts/White/White_Rotate.cs
@@ -6,9 +6,6 @@ public class White_Rotate : MonoBehaviour
 {
     void Start()
     {
-        //float yAngle = Random.Range(-180, 180f);
-        //transform.Rotate(0, yAngle, 0);
-
         var euler = transform.eulerAngles;
         euler.y = Random.Range(0f, 360f);
         transform.eulerAngles = euler;
