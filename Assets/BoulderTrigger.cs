5c18112762c9f10322a34eb7c3870254d26045e9
Keegan <johnk117@ferris.edu>
2019-09-19 12:06:51 -0400

    Worked on ChunKJohnson
    + old radio tower
    + Boulders
    + Was working on trigger to make them fall

---
+++ Assets/BoulderTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderTrigger : MonoBehaviour
+{
+
+    public Rigidbody[] rb;
+    bool isCollided = false;
+
+    void Start()
+    {
+        rb = GetComponents<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isCollided)
+        {
+            foreach (Rigidbody rigid in rb)
+            {
+                rigid.isKinematic = false;
+            }
+            Debug.Log("hello");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isCollided = true;
+    }
+}
