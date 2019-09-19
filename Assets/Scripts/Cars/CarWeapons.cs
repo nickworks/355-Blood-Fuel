dd4128aedad828efbd166def47880c0d43381617
Nick <pattison.nick@gmail.com>
2019-09-19 17:05:39 -0400

    Added CarWeapons class
    + CarWeapons : This should serve as a car's "weapon system"
    + RocketLauncher prefab
    * Renamed "Mouse ScrollWheel" input axis to "ScrollWeapon"

---
+++ Assets/Scripts/Cars/CarWeapons.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarWeapons : MonoBehaviour {
+
+    public Transform weaponSlot;
+    public Weapon[] weaponLoadout;
+    int weaponIndex = 0;
+    Weapon currentWeapon;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+    void NextWeapon() {
+        weaponIndex++;
+        LoadWeapon();
+    }
+    void PrevWeapon() {
+        weaponIndex--;
+        LoadWeapon();
+    }
+    void LoadWeapon() {
+        if (weaponIndex < 0) weaponIndex = weaponLoadout.Length - 1;
+        if (weaponIndex < 0) return; // no weapons...
+        if (weaponIndex == weaponLoadout.Length) weaponIndex = 0;
+
+        Weapon nextWeapon = weaponLoadout[weaponIndex];
+
+        if (nextWeapon == null) return; // weapon is null...
+
+        if (currentWeapon.gameObject != null) Destroy(currentWeapon);
+
+        currentWeapon = Instantiate(nextWeapon, weaponSlot);
+    }
+}
