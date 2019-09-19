6da5e8ab41a1fb0bacc28c681c66a9d180f29934
Nick <pattison.nick@gmail.com>
2019-09-19 13:51:34 -0400

    Fixed a few bugs
    * Fixed issue with duplicate AnimationChance script on prefabs in Wiles' chunk
    * Fixed issue with notifications in an Animation in White's chunk

--- Assets/Scripts/Wiles/AnimationChance.cs
+++ Assets/Scripts/Wiles/AnimationChance.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class AnimationChance : MonoBehaviour
 {
+    //DONE: Setup a 1 in 100 chance that this object will play an animation when the player gets close.
+    //TODO: Allow this script to "see" the distance from this object to any and all cars. If one gets close, play the animation.
+
+    bool willAnim = false;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        if (Random.Range(1, 100) == 50) willAnim = true;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (willAnim) {
+            //Check the position of all of the cars. See if any is close enough. If so, play the animation.
+        }
     }
 }
