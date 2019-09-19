7ab168aed2d2733f96117c3e75e323fc0c314388
jesse <jesse199323@yahoo.com>
2019-09-19 02:59:45 -0400

    made more caves and made it so some objects are destroyed
    Added a destroy script called spawn
    Added 2 more terrains
    Added 3 more walls

---
+++ Assets/Scripts/Webb/Spawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    int death = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        death += Random.Range(0, 3);
+        if (death > 0)
+        {
+            Destroy();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Destroy()
+    {
+
+        Destroy(gameObject);
+    }
+}
