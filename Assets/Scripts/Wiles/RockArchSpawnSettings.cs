10713c0d690eeee66b5d89a3a1bc03e5d1bb9918
Jacob Wiles <Awsoemman328@gmail.com>
2019-09-19 12:54:15 -0400

    Adjusted Rock Arches randomization so now it works properly now
    *Rock Arches can now move slightly in addition to being slightly rotated.
    *Changed The LevelGen so that it spawns all of our chunks again.

--- Assets/Scripts/Wiles/RockArchSpawnSettings.cs
+++ Assets/Scripts/Wiles/RockArchSpawnSettings.cs
@@ -8,15 +8,18 @@ public class RockArchSpawnSettings : MonoBehaviour
     public float horizontalNudge;
     public float verticalNudge;
     public float rotateAmount;
-    Transform tempTrans;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        tempTrans = transform;
-        //tempTrans.localPosition = new Vector3(Random.Range(-horizontalNudge, horizontalNudge), 0, Random.Range(-verticalNudge, verticalNudge));
-        tempTrans.localEulerAngles = new Vector3(0, Random.Range(-rotateAmount, rotateAmount), 11.195f);
-        //Transform Transform = tempTrans;
+        var tempPosition = transform.position;
+        tempPosition.x = tempPosition.x + Random.Range(-horizontalNudge, horizontalNudge);
+        tempPosition.z = tempPosition.z + Random.Range(-verticalNudge, verticalNudge);
+        var tempAngle = transform.eulerAngles;
+        tempAngle = new Vector3(0, Random.Range(-rotateAmount, rotateAmount), 11.195f);
+        transform.position = tempPosition;
+        transform.eulerAngles = tempAngle;
     }
 
     // Update is called once per frame
