409d1e77388ec7516187c4a42892bc16e8e52a78
Keegan <johnk117@ferris.edu>
2019-09-21 00:05:25 -0400

    Worked on level staging
    + extra terrain
    + Boulder Trigger script
    
    ~ I expanded the length of ChunkJohnson, so it is now 2 km by 1 km. I also am working on the terrain. I still need to add barrels in. I also want to add some traps to the level, like a pole that pops up if player gets within certain range.....

--- Assets/BoulderTrigger.cs
+++ Assets/BoulderTrigger.cs
@@ -6,29 +6,24 @@ public class BoulderTrigger : MonoBehaviour
 {
 
     public Rigidbody[] rb;
-    bool isCollided = false;
 
     void Start()
     {
-        rb = GetComponents<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isCollided)
-        {
-            foreach (Rigidbody rigid in rb)
-            {
-                rigid.isKinematic = false;
-            }
-            Debug.Log("hello");
-        }
+         
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        isCollided = true;
+        foreach (Rigidbody rigid in rb)
+        {
+            rigid.isKinematic = false;
+            
+        }
     }
 }
--- Assets/Scripts/Johnson/RotatingJohnson.cs
+++ Assets/Scripts/Johnson/RotatingJohnson.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotating : MonoBehaviour
+public class RotatingJohnson : MonoBehaviour
 {
 
     // Start is called before the first frame update
@@ -20,21 +20,21 @@ public class Rotating : MonoBehaviour
     // rotates prefab by a random 90 degrees
     void RandomRotation()
     {
-        float randPicker = Random.Range(0f, 1f);
+        float randPicker = Random.Range(0f, 1.3f);
 
         if (randPicker < .3f)
         {
             gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
         }
-        if (randPicker > .3f && randPicker < .5f)
+        if (randPicker > .3f && randPicker < .6f)
         {
             gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
         }
-        if (randPicker > .5f && randPicker < .7f)
+        if (randPicker > .6f && randPicker < .9f)
         {
             gameObject.transform.rotation = Quaternion.Euler(0, 270, 0);
         }
-        if (randPicker > .7f)
+        if (randPicker > 1.2f)
         {
             gameObject.transform.rotation = Quaternion.Euler(0, 360, 0);
         }
