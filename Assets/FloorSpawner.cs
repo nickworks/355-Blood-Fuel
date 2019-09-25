d8078457df696da07aa5365a34d6a662a24c7305
Unknown <webbj19@ferris.edu>
2019-09-25 15:11:11 -0400

    made a ramp and a script that checks for other objects
    Added script that destroys if other object exist
    Added a Terrain for start of level
    edited terrain to ramp

---
+++ Assets/FloorSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorSpawner : MonoBehaviour
+{
+     int timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GroundCheck();
+    }
+    void GroundCheck()
+    {
+        timer += 1;
+        if (timer >= 0)
+        {
+            if (GameObject.Find("groundStart") != null)
+            {
+
+                Destroy(gameObject);
+
+            }
+        }
+    }
+}
